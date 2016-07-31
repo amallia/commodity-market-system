f74e0f27e8766ae6405424568743614058223fcd
amallia <me@antoniomallia.it>
2016-07-31 20:53:14 +0200

    Changed string compare function

--- src/cms.cpp
+++ src/cms.cpp
@@ -10,7 +10,7 @@
 #include "utils.h"
 
 
-const std::string header = "                                  \n"
+static const std::string header = "                                  \n"
         "  /$$$$$$$ /$$$$$$/$$$$   /$$$$$$$\n"
         " /$$_____/| $$_  $$_  $$ /$$_____/\n"
         "| $$      | $$ \\ $$ \\ $$|  $$$$$$ \n"
@@ -18,7 +18,7 @@ const std::string header = "                                  \n"
         "|  $$$$$$$| $$ | $$ | $$ /$$$$$$$/\n"
         " \\_______/|__/ |__/ |__/|_______/ \n";
 
-const std::string welcomeMsg = "Welcome to Commodity Market System - CMS<GO>";
+static const std::string welcomeMsg = "Welcome to Commodity Market System - CMS<GO>";
 
 int main(int argc, char *argv[]) {
     try {
@@ -27,9 +27,9 @@ int main(int argc, char *argv[]) {
         std::cout << welcomeMsg << std::endl;
         if (argc == 2 && strcmp(argv[1], "base") == 0) {
             input_ptr = boost::shared_ptr<cms::Input>(new cms::CLI());
-        } else if (argc == 3 && boost::equals(std::string(std::string(argv[1])), "ext1") && cms::isInteger(argv[2])) {
+        } else if (argc == 3 && strcmp(argv[1], "ext1") && cms::isInteger(argv[2])) {
             input_ptr = boost::shared_ptr<cms::Input>(new cms::NetworkInput(cms::toInt(argv[2])));
-        } else if (argc == 3 && boost::equals(std::string(std::string(argv[1])), "ext2") && cms::isInteger(argv[2])) {
+        } else if (argc == 3 && strcmp(argv[1], "ext2") && cms::isInteger(argv[2])) {
             input_ptr = boost::shared_ptr<cms::Input>(new cms::MultiThreadInput(cms::toInt(argv[2])));
         } else {
             std::cout << "Usage: ./cms base|ext1|ext2 <portNo>" << std::endl;
